48d5aa74758da6c74a2ff5f23b389cbdf7810c17
EgiKlein <masterp7@gmail.com>
2021-07-15 11:27:49 +0300

    First commit

---
+++ class30.js
@@ -0,0 +1,8 @@
+//Jquery UI - Accordion Example
+$( () => {
+    $( "#accordion" ).accordion();
+    $( "#accordion" ).on( "accordionactivate", function( event, ui ) {
+        $(ui.newPanel).css("background-color","Yellow");
+        $(ui.oldPanel).css("background-color","blue");
+    } );
+  } );
